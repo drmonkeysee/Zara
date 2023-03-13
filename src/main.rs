77316569f8ad90df3f8aeb9010854e77bed5dbf4
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-03-12 17:49:29 -0700

    replace args code with clap, print script arg metadata

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,22 @@
-fn main() {
-    let mut name = String::from(zara::LIB_INFO.name);
-    make_titlecase(&mut name);
-    let args = parse_args(std::env::args());
-    if args.version {
-        print_version(&name);
-    } else {
-        print_usage(&args.me, &name);
+use clap::Parser;
+use std::error::Error;
+use std::fs::File;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+    println!("Input: {cli:?}");
+    if let Some(p) = cli.script_path.as_ref() {
+        let md = File::open(p)?.metadata()?;
+        println!("File data is {md:?}");
     }
+    Ok(())
 }
 
-struct ParsedArgs {
-    me: String,
-    version: bool,
-}
-
-fn parse_args(mut args: impl Iterator<Item = String>) -> ParsedArgs {
-    let mut parsed = ParsedArgs {
-        me: args.next().unwrap_or(String::from(zara::LIB_INFO.name)),
-        version: false,
-    };
-    for arg in args {
-        match arg.as_str() {
-            "-V" | "--version" => parsed.version = true,
-            _ => (),
-        }
-    }
-    parsed
-}
-
-fn make_titlecase(n: &mut str) {
-    if let Some(t) = n.get_mut(0..1) {
-        t.make_ascii_uppercase()
-    }
-}
-
-fn print_version(n: &str) {
-    println!("{n} {}", zara::LIB_INFO.version.full);
-}
-
-fn print_usage(me: &str, n: &str) {
-    println!("---=== {n} Usage ===---");
-    println!("{me} [options]");
+#[derive(Debug, Parser)]
+#[command(about, version)]
+struct Cli {
+    #[arg(value_name = "FILE")]
+    /// Script file to execute.
+    script_path: Option<PathBuf>,
 }
