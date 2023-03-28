a61f8dc057c060002a8bc5213690184791c58b3a
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-03-28 16:29:49 -0700

    wire up rustyline with very basic repl

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 pub fn lib_hello() {
     println!("Hello from Zara!");
 }
+
+pub fn eval(text: &str) -> String {
+    let mut s = String::from("Evaluated text: ");
+    s.push_str(text);
+    s
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
+use rustyline::{DefaultEditor, Result};
+
+fn main() -> Result<()> {
     println!("Starting zara cli...");
     zara::lib_hello();
+    repl()
+}
+
+fn repl() -> Result<()> {
+    let mut ed = DefaultEditor::new()?;
+    for readline in ed.iter("λ:> ") {
+        let line = readline?;
+        let result = zara::eval(line.as_str());
+        println!("{result}");
+    }
+    eprintln!("Saw EOF");
+    Ok(())
 }
