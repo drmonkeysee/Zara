48e2e9a128b497c773dbd28acb148bd5894efa25
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-05-06 17:55:46 -0700

    stub out parse step

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 mod lex;
 mod literal;
+mod syntax;
 
 use self::lex::LexerResult;
 
 pub fn eval(textline: String) -> Evaluation {
-    let result = lex::tokenize(&textline);
+    let tokens = lex::tokenize(&textline);
+    let ast = syntax::parse(&tokens, &textline);
     Evaluation {
         textline,
-        lex_output: result,
+        lex_output: tokens,
     }
 }
 
---
+++ src/syntax.rs
@@ -0,0 +1,6 @@
+use crate::LexerResult;
+
+// TODO: can tokens be taken by value later
+pub(super) fn parse(tokens: &LexerResult, textline: &str) {
+    todo!("parse token stream");
+}
