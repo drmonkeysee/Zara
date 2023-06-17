5abbc6dc09b3efeb8acf50c8d09cddc0f97e63ee
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-06-17 15:16:56 -0700

    split up interprerter and eval results, stub out evaluate of expression list

--- src/lib.rs
+++ src/lib.rs
@@ -7,30 +7,53 @@ use self::{
     syntax::{Expression, ParserFailure},
 };
 
-pub fn eval(textline: String) -> EvalResult {
-    evaluate(syntax::parse(lex::tokenize(&textline)?.into_iter())?.into_iter())
+pub fn eval(textline: String) -> InterpreterResult {
+    Ok(evaluate(
+        syntax::parse(lex::tokenize(&textline)?.into_iter())?.into_iter(),
+    )?)
 }
 
 #[derive(Debug)]
-pub enum EvalError {
+pub enum InterpreterError {
     Lexer(LexerFailure),
     Parser(ParserFailure),
+    Eval(EvalError),
 }
+
+type InterpreterResult = Result<Expression, InterpreterError>;
+
+#[derive(Debug)]
+pub struct EvalError;
 
 type EvalResult = Result<Expression, EvalError>;
 
 fn evaluate(expressions: impl Iterator<Item = Expression>) -> EvalResult {
-    Ok(expressions.last().unwrap())
+    // TODO: use reduce or try_for_each
+    let mut result = Err(EvalError);
+    for expr in expressions {
+        result = eval_expr(expr)
+    }
+    result
 }
 
-impl From<LexerFailure> for EvalError {
+fn eval_expr(expr: Expression) -> EvalResult {
+    Ok(expr)
+}
+
+impl From<LexerFailure> for InterpreterError {
     fn from(value: LexerFailure) -> Self {
         Self::Lexer(value)
     }
 }
 
-impl From<ParserFailure> for EvalError {
+impl From<ParserFailure> for InterpreterError {
     fn from(value: ParserFailure) -> Self {
         Self::Parser(value)
+    }
+}
+
+impl From<EvalError> for InterpreterError {
+    fn from(value: EvalError) -> Self {
+        Self::Eval(value)
     }
 }
