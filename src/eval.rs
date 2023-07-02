36e7bc8927751dd83e5de9f8199936f1dd265161
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-07-02 16:39:24 -0700

    pull begin eval into helper function

--- src/eval.rs
+++ src/eval.rs
@@ -9,13 +9,15 @@ pub(super) fn evaluate(expression: Expression) -> EvalResult {
     // TODO: support CLI flag for outputing Abstract Syntax Tree expression
     //return Ok(Expression::Ast(Box::new(expression)));
     match expression {
-        Expression::Begin(exprs) => exprs
-            .into_iter()
-            .try_fold(Expression::Empty, |_, expr| eval_expr(expr)),
+        Expression::Begin(exprs) => eval_begin(exprs.into_iter()),
         _ => Err(EvalError),
     }
 }
 
 fn eval_expr(expr: Expression) -> EvalResult {
     Ok(expr)
+}
+
+fn eval_begin(mut exprs: impl Iterator<Item = Expression>) -> EvalResult {
+    exprs.try_fold(Expression::Empty, |_, expr| eval_expr(expr))
 }
