8aef9ce7945a81bafca15064a8dcc8f80e534a38
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-07-08 18:03:04 -0700

    add display for tokens

--- src/lex/tokens.rs
+++ src/lex/tokens.rs
@@ -1,5 +1,8 @@
 use crate::literal::Literal;
-use std::ops::Range;
+use std::{
+    fmt::{Display, Error, Formatter},
+    ops::Range,
+};
 
 pub(crate) type Token = TokenType<TokenKind>;
 pub(super) type TokenError = TokenType<TokenErrorKind>;
@@ -28,4 +31,69 @@ pub(crate) enum TokenErrorKind {
 pub struct TokenType<T> {
     pub(crate) kind: T,
     pub(crate) span: Range<usize>,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{}[{}..{}]", self.kind, self.span.start, self.span.end)
+    }
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        match self {
+            Self::Literal(lit) => write!(f, "LITERAL({:?})", lit),
+            Self::ParenLeft => f.write_str("LPAREN"),
+            Self::ParenRight => f.write_str("RPAREN"),
+            Self::VectorOpen => f.write_str("OPENVEC"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display_literal() {
+        let token = Token {
+            kind: TokenKind::Literal(Literal::Boolean(true)),
+            span: Range { start: 0, end: 2 },
+        };
+
+        assert_eq!(
+            token.to_string(),
+            format!("LITERAL({:?})[0..2]", Literal::Boolean(true))
+        );
+    }
+
+    #[test]
+    fn display_paren_left() {
+        let token = Token {
+            kind: TokenKind::ParenLeft,
+            span: Range { start: 0, end: 1 },
+        };
+
+        assert_eq!(token.to_string(), "LPAREN[0..1]");
+    }
+
+    #[test]
+    fn display_paren_right() {
+        let token = Token {
+            kind: TokenKind::ParenRight,
+            span: Range { start: 0, end: 1 },
+        };
+
+        assert_eq!(token.to_string(), "RPAREN[0..1]");
+    }
+
+    #[test]
+    fn display_vector_open() {
+        let token = Token {
+            kind: TokenKind::VectorOpen,
+            span: Range { start: 0, end: 2 },
+        };
+
+        assert_eq!(token.to_string(), "OPENVEC[0..2]");
+    }
 }
