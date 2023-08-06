1541f13aebe476c1f0937f90a0bca5ba0fb4b79d
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-08-05 21:08:20 -0700

    implement text source for repl

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod args;
 
 use self::args::{Opts, Parsed};
 use rustyline::{DefaultEditor, Result};
-use std::env;
-use zara::{Evaluation, Expression, Interpreter};
+use std::{env, rc::Rc};
+use zara::{
+    txt::{LineNumber, TextContext, TextLine, TextSource},
+    Error, Evaluation, Expression, Interpreter,
+};
 
 const INPUT: &str = "λ:> ";
 const CONT: &str = "... ";
@@ -22,8 +25,10 @@ fn main() -> Result<()> {
 }
 
 struct Repl {
+    ctx: Rc<TextContext>,
     editor: DefaultEditor,
     interpreter: Interpreter,
+    lineno: LineNumber,
     options: Opts,
     prompt: &'static str,
     running: bool, // TODO: will be needed for repl quit
@@ -32,8 +37,10 @@ struct Repl {
 impl Repl {
     fn new(options: Opts) -> Result<Self> {
         Ok(Self {
+            ctx: Rc::new(TextContext::named(String::from("<repl>"))),
             editor: DefaultEditor::new()?,
             interpreter: Interpreter::new(),
+            lineno: 1,
             options,
             prompt: INPUT,
             running: true,
@@ -50,12 +57,17 @@ impl Repl {
     }
 
     fn runline(&mut self, line: String) {
-        match self.interpreter.runline(line) {
+        let mut src = ReplSource {
+            ctx: self.ctx.clone(),
+            line: Some(line),
+            lineno: self.lineno,
+        };
+        match self.interpreter.run(&mut src) {
             Ok(eval) => match eval {
-                Evaluation::Continuation => self.prompt = CONT,
+                Evaluation::Continuation => self.prep_continuation(),
                 Evaluation::Expression(expr) => self.print_expr(expr),
             },
-            Err(err) => eprint!("{}", err.verbose_display()),
+            Err(err) => self.print_err(err),
         }
     }
 
@@ -63,6 +75,50 @@ impl Repl {
         if expr.has_repr() {
             println!("==> {}", expr)
         }
+        self.reset();
+    }
+
+    fn print_err(&mut self, err: Error) {
+        eprint!("{}", err.verbose_display());
+        self.reset();
+    }
+
+    fn prep_continuation(&mut self) {
+        self.prompt = CONT;
+        self.lineno += 1;
+    }
+
+    fn reset(&mut self) {
         self.prompt = INPUT;
+        self.lineno = 1;
+    }
+}
+
+struct ReplSource {
+    ctx: Rc<TextContext>,
+    line: Option<String>,
+    lineno: LineNumber,
+}
+
+// TODO: can this be a macro
+impl TextSource for ReplSource {
+    fn context(&self) -> Rc<TextContext> {
+        self.ctx.clone()
+    }
+}
+
+impl Iterator for ReplSource {
+    type Item = TextLine;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(line) = self.line.take() {
+            Some(TextLine {
+                ctx: self.context(),
+                line,
+                lineno: self.lineno,
+            })
+        } else {
+            None
+        }
     }
 }
