1abe455949f445acdd96ba1891670abbff0de8a8
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-08-27 13:49:29 -0700

    add exit wrapper for result to control output better

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,34 @@ mod repl;
 mod run;
 
 use self::cmd::{Cmd, Result};
-use std::env;
+use std::{
+    env,
+    process::{ExitCode, Termination},
+};
 
-fn main() -> Result {
+fn main() -> Exit {
     let cmd: Cmd = args::parse(env::args()).into();
-    cmd.execute()
+    cmd.execute().into()
+}
+
+// NOTE: newtype to have more control over exit output rather than the
+// annoying default behavior of Result printing the Debug representation.
+struct Exit(Result);
+
+impl From<Result> for Exit {
+    fn from(value: Result) -> Self {
+        Self(value)
+    }
+}
+
+impl Termination for Exit {
+    fn report(self) -> ExitCode {
+        self.0.map_or_else(
+            |err| {
+                eprintln!("{}", err);
+                ExitCode::FAILURE
+            },
+            |u| u.report(),
+        )
+    }
 }
