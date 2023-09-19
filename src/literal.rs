165b466f55293ecc9280d48d670f74f9aae8274b
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-09-18 21:18:00 -0700

    clarify string literal tests

--- src/literal.rs
+++ src/literal.rs
@@ -220,13 +220,106 @@ mod tests {
         }
 
         #[test]
-        fn display_escape_sequences() {
-            let s = Literal::String("al: \\a, tab: \\t, nl: \\n, sl: \\\\, q: \\\"".to_owned());
+        fn display_extended() {
+            let s = Literal::String("λ".to_owned());
 
-            assert_eq!(
-                s.as_datum().to_string(),
-                "\"al: \\a, tab: \\t, nl: \\n, sl: \\\\, q: \\\"\""
+            assert_eq!(s.as_datum().to_string(), "\"λ\"");
+        }
+
+        #[test]
+        fn display_emoji() {
+            let s = Literal::String("🦀".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"🦀\"");
+        }
+
+        #[test]
+        fn display_control_picture() {
+            let s = Literal::String("\u{2401}".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"␁\"");
+        }
+
+        #[test]
+        fn display_replacement() {
+            let s = Literal::String("\u{fffd}".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"�\"");
+        }
+
+        #[test]
+        fn display_one_digit_hex() {
+            let s = Literal::String("\x0c".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"\\xc;\"");
+        }
+
+        #[test]
+        fn display_hex_uses_lowercase() {
+            let s = Literal::String("\x0C".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"\\xc;\"");
+        }
+
+        #[test]
+        fn display_two_digit_hex() {
+            let s = Literal::String("\x1d".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"\\x1d;\"");
+        }
+
+        #[test]
+        fn display_four_digit_hex() {
+            let s = Literal::String("\u{fff9}".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"\\xfff9;\"");
+        }
+
+        #[test]
+        fn display_special_purpose_plane() {
+            let s = Literal::String("\u{e0001}".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"\\xe0001;\"");
+        }
+
+        #[test]
+        fn display_private_use_plane() {
+            let s = Literal::String("\u{100001}".to_owned());
+
+            assert_eq!(s.as_datum().to_string(), "\"\\x100001;\"");
+        }
+
+        #[test]
+        fn display_literal_endline() {
+            let s = Literal::String(
+                "foo
+bar"
+                .to_owned(),
             );
+
+            assert_eq!(s.as_datum().to_string(), "\"foo\\nbar\"");
+        }
+
+        #[test]
+        fn display_escape_sequences() {
+            check_escape_sequence(&[
+                ("\x07", "\\a"),
+                ("\x08", "\\b"),
+                ("\t", "\\t"),
+                ("\n", "\\n"),
+                ("\r", "\\r"),
+                ("\"", "\\\""),
+                ("\\", "\\\\"),
+                ("\\|", "\\|"),
+            ]);
+        }
+
+        fn check_escape_sequence(cases: &[(&str, &str)]) {
+            for &(inp, exp) in cases {
+                let s = Literal::String(inp.to_owned());
+
+                assert_eq!(s.as_datum().to_string(), format!("\"{exp}\""));
+            }
         }
     }
 }
