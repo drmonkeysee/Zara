3423832a1505aa2aaea00383b8e392caaab53300
Brandon Stansbury <brandonrstansbury@gmail.com>
2023-09-26 19:03:51 -0700

    generalize file source into line stream source

--- src/src.rs
+++ src/src.rs
@@ -1,10 +1,32 @@
 use crate::txt::{LineNumber, TextContext, TextLine, TextSource};
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Result},
+    io::{BufRead, BufReader, IsTerminal, Result},
     path::Path,
     rc::Rc,
 };
+
+pub type FileSource = LineInputSource<FileLineAdapter>;
+
+impl FileSource {
+    pub fn file(path: impl AsRef<Path>) -> Result<Self> {
+        let name = path
+            .as_ref()
+            .to_str()
+            .unwrap_or("#<invalid-file-path>")
+            .to_owned();
+        let f = File::open(path)?;
+        Self::init(f, name)
+    }
+
+    fn init(f: File, n: String) -> Result<Self> {
+        Ok(Self {
+            adapter: FileLineAdapter(BufReader::new(f)),
+            ctx: TextContext::named(n).into(),
+            lineno: 1,
+        })
+    }
+}
 
 pub struct StringSource {
     ctx: Rc<TextContext>,
@@ -46,53 +68,48 @@ impl TextSource for StringSource {
     }
 }
 
-pub struct FileSource {
+// NOTE: adapter type to bridge types like BufReader<T> and Stdin which
+// *almost* implement the same interface but not quite.
+pub trait LineInputAdapter {
+    fn is_tty(&self) -> bool;
+    fn read_line(&mut self, buf: &mut String) -> Result<usize>;
+}
+
+pub struct FileLineAdapter(BufReader<File>);
+
+impl LineInputAdapter for FileLineAdapter {
+    fn is_tty(&self) -> bool {
+        self.0.get_ref().is_terminal()
+    }
+
+    fn read_line(&mut self, buf: &mut String) -> Result<usize> {
+        self.0.read_line(buf)
+    }
+}
+
+pub struct LineInputSource<T> {
+    adapter: T,
     ctx: Rc<TextContext>,
     lineno: LineNumber,
-    reader: BufReader<File>,
 }
 
-impl FileSource {
-    pub fn file(path: impl AsRef<Path>) -> Result<Self> {
-        let name = path
-            .as_ref()
-            .to_str()
-            .unwrap_or("#<invalid-file-path>")
-            .to_owned();
-        let f = File::open(path)?;
-        Self::init(f, name)
-    }
-
-    pub fn library(name: impl Into<String>) -> Result<Self> {
-        todo!();
-    }
-
-    fn init(f: File, n: String) -> Result<Self> {
-        Ok(Self {
-            ctx: TextContext::named(n).into(),
-            lineno: 1,
-            reader: BufReader::new(f),
-        })
-    }
-}
-
-impl Iterator for FileSource {
+impl<T: LineInputAdapter> Iterator for LineInputSource<T> {
     type Item = TextLine;
 
     fn next(&mut self) -> Option<Self::Item> {
         let lineno = self.lineno;
         self.lineno += 1;
         let mut buf = String::new();
-        self.reader.read_line(&mut buf).map_or_else(
+        self.adapter.read_line(&mut buf).map_or_else(
             |err| {
                 eprintln!(
-                    "{}:{lineno}\n\tunexpected file read error: {err}",
+                    "{}:{lineno}\n\tunexpected line read error: {err}",
                     self.ctx.name
                 );
                 None
             },
             |n| {
-                if n == 0 {
+                if n == 0 || (n == 1 && self.adapter.is_tty()) {
                     None
                 } else {
                     // NOTE: read_line guarantees a trailing \n, safe to pop
@@ -108,7 +125,7 @@ impl Iterator for FileSource {
     }
 }
 
-impl TextSource for FileSource {
+impl<T: LineInputAdapter> TextSource for LineInputSource<T> {
     fn context(&self) -> Rc<TextContext> {
         self.ctx.clone()
     }
