30750bf4f73a70989c9754573ca9fa74b9fd5cca
Brandon Stansbury <brandonrstansbury@gmail.com>
2025-05-25 18:31:09 -0700

    stub out rest of time library

--- src/core/time.rs
+++ src/core/time.rs
@@ -6,7 +6,13 @@ use crate::{
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub(super) fn load(env: &mut Frame) {
+    super::bind_intrinsic(env, "current-jiffy", 0..0, current_jiffy);
     super::bind_intrinsic(env, "current-second", 0..0, current_second);
+    super::bind_intrinsic(env, "jiffies-per-second", 0..0, jiffies_per_second);
+}
+
+fn current_jiffy(_args: &[ValueRef], _env: &Frame) -> EvalResult {
+    todo!("current-jiffy");
 }
 
 fn current_second(_args: &[ValueRef], _env: &Frame) -> EvalResult {
@@ -18,4 +24,8 @@ fn current_second(_args: &[ValueRef], _env: &Frame) -> EvalResult {
         },
         |d| Ok(Value::number(d.as_secs_f64()).into()),
     )
+}
+
+fn jiffies_per_second(_args: &[ValueRef], _env: &Frame) -> EvalResult {
+    todo!("jiffies-per-second");
 }
