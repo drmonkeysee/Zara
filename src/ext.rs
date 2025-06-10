04660c1a1288edf653603734e89fa5abab5d1834
Brandon Stansbury <brandonrstansbury@gmail.com>
2025-06-09 19:04:28 -0700

    rename symbol proc and mark as infallible

--- src/ext.rs
+++ src/ext.rs
@@ -13,14 +13,15 @@ use std::rc::Rc;
  */
 
 pub(crate) fn load(scope: &mut Binding) {
-    core::bind_intrinsic(scope, "zara-symbol-table", 0..0, symbol_table);
+    core::bind_intrinsic(scope, "zara-symbols", 0..0, symbols);
 
     // TODO: test variables
     scope.bind("x", Value::real(5));
     scope.bind("z", Value::Unspecified);
 }
 
-fn symbol_table(_args: &[Value], env: &mut Frame) -> EvalResult {
+#[allow(clippy::unnecessary_wraps, reason = "infallible intrinsic")]
+fn symbols(_args: &[Value], env: &mut Frame) -> EvalResult {
     Ok(Value::list(
         env.sym
             .get_refs()
