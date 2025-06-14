9397b90d40a12f946599be52e9ffec4a7319388f
Brandon Stansbury <brandonrstansbury@gmail.com>
2025-06-14 13:25:59 -0700

    factor out predicate procedures into macro

--- src/core/base.rs
+++ src/core/base.rs
@@ -1,4 +1,13 @@
 // (scheme base)
+macro_rules! predicate {
+    ($name:ident, $pred:pat $(if $guard:expr)? $(,)?) => {
+        fn $name(args: &[Value], _env: &mut Frame) -> EvalResult {
+            let arg = args.first().unwrap();
+            Ok(Value::Boolean(matches!(arg, $pred $(if $guard)?)))
+        }
+    };
+}
+
 use crate::{
     eval::{Binding, EvalResult, Frame, MAX_ARITY},
     number::{Number, Real},
@@ -30,10 +39,8 @@ pub(super) fn load(scope: &mut Binding) {
 // Boolean
 //
 
-fn is_boolean(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(arg, Value::Boolean(_))))
-}
+predicate!(is_boolean, Value::Boolean(_));
+predicate!(not, Value::Boolean(false));
 
 fn all_boolean(args: &[Value], _env: &mut Frame) -> EvalResult {
     Ok(Value::Boolean(
@@ -41,88 +48,20 @@ fn all_boolean(args: &[Value], _env: &mut Frame) -> EvalResult {
     ))
 }
 
-fn not(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(arg, Value::Boolean(false))))
-}
-
 //
 // Number
 //
 
-fn is_exact(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(n) if !n.is_inexact()
-    )))
-}
-
-fn is_exact_integer(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(Number::Real(Real::Integer(_)))
-    )))
-}
-
-fn is_finite(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(n) if !n.is_infinite() && !n.is_nan()
-    )))
-}
-
-fn is_inexact(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(n) if n.is_inexact()
-    )))
-}
-
-fn is_infinite(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(n) if n.is_infinite()
-    )))
-}
-
-fn is_integer(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(Number::Real(r)) if r.is_integer()
-    )))
-}
-
-fn is_nan(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(n) if n.is_nan()
-    )))
-}
-
-fn is_number(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(arg, Value::Number(_))))
-}
-
-fn is_rational(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(Number::Real(r)) if r.is_rational()
-    )))
-}
-
-fn is_real(args: &[Value], _env: &mut Frame) -> EvalResult {
-    let arg = args.first().unwrap();
-    Ok(Value::Boolean(matches!(
-        arg,
-        Value::Number(Number::Real(_))
-    )))
-}
+predicate!(is_exact, Value::Number(n) if !n.is_inexact());
+predicate!(
+    is_exact_integer,
+    Value::Number(Number::Real(Real::Integer(_))),
+);
+predicate!(is_finite, Value::Number(n) if !n.is_infinite() && !n.is_nan());
+predicate!(is_inexact, Value::Number(n) if n.is_inexact());
+predicate!(is_infinite, Value::Number(n) if n.is_infinite());
+predicate!(is_integer, Value::Number(Number::Real(r)) if r.is_integer());
+predicate!(is_nan, Value::Number(n) if n.is_nan());
+predicate!(is_number, Value::Number(_));
+predicate!(is_rational, Value::Number(Number::Real(r)) if r.is_rational());
+predicate!(is_real, Value::Number(Number::Real(_)));
