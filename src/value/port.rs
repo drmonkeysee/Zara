fe47203d0d24bda6830990fe3d1b3620b446a593
Brandon Stansbury <brandonrstansbury@gmail.com>
2025-12-06 21:47:21 -0800

    get basic datum lexing working for ports

--- src/value/port.rs
+++ src/value/port.rs
@@ -1,12 +1,12 @@
 use super::{TypeName, Value};
 use crate::{
-    eval::Frame,
-    lex::Lexer,
+    eval::{Frame, Namespace},
+    lex::{Lexer, LexerOutput},
     string::{
-        SymbolTable,
+        self, SymbolTable, is_whitespace,
         unicode::{self, UnicodeError},
     },
-    syntax::ExpressionTree,
+    syntax::{ExpressionTree, Parser, ParserOutput},
 };
 use std::{
     cmp,
@@ -132,8 +132,7 @@ impl ReadPort {
     }
 
     pub(crate) fn read_datum(&mut self, env: &Frame) -> PortDatum {
-        let mut r = DatumReader::default();
-        r.parse(self, env)
+        parse_datum(self.get_char_reader_mut()?, env)
     }
 
     pub(crate) fn read_byte(&mut self) -> PortByte {
@@ -1084,40 +1083,137 @@ impl CharReader for StringReader {
     }
 }
 
-#[derive(Default)]
-struct DatumReader {
-    lex: Lexer,
-    parse: ExpressionTree,
+enum ScanEnd {
+    Character,
+    DoubleQuote,
+    Paren,
+    Pipe,
 }
 
-impl DatumReader {
-    fn parse(&mut self, port: &mut ReadPort, env: &Frame) -> PortDatum {
-        let r = port.get_char_reader_mut()?;
-        let mut buf = String::new();
-        loop {
-            /*
-             * peek ch
-             * if none -> determine error state (incomplete or EOF)
-             * if some
-             *  skip leading whitespace
-             *  read chars until delimiter
-             *  lex buffer
-             *      if continuation
-             *          get next char and relex
-             *          if still continuation read to next delimiter
-             *  if lexed
-             *      parse
-             *          if continuation act as above with lex
-             *  if parsed empty result
-             *      keep going
-             *  else return datum
-             *
-             *  notes:
-             *      - can always read chars until token start
-             */
+impl ScanEnd {
+    fn scan(&self, r: &mut dyn CharReader, buf: &mut String) -> PortResult {
+        match self {
+            Self::Character => {
+                while let Some(ch) = r.peek_char()? {
+                    if !string::is_delimiter(ch) {
+                        consume_char(r, buf)?;
+                    }
+                }
+                Ok(())
+            }
+            Self::DoubleQuote => read_to(r, '"', buf),
+            Self::Paren => read_to(r, ')', buf),
+            Self::Pipe => read_to(r, '|', buf),
         }
-        todo!();
     }
+}
+
+fn parse_datum(r: &mut dyn CharReader, env: &Frame) -> PortDatum {
+    let mut buf = String::new();
+    let Some(end) = start_scan(r, &mut buf)? else {
+        return Ok(None);
+    };
+    end.scan(r, &mut buf)?;
+    /*
+    let lex = Lexer::default();
+    let expr = ExpressionTree::default();
+    match lex.tokenize(src)? {
+        LexerOutput::Complete(tokens) => match expr.parse(tokens, Namespace(env.new_child()))? {
+            ParserOutput::Complete(datum) => todo!(),
+            ParserOutput::Continuation => todo!(),
+        },
+        LexerOutput::Continuation => todo!(),
+    }
+    */
+    todo!();
+}
+
+fn start_scan(r: &mut dyn CharReader, buf: &mut String) -> PortResult<Option<ScanEnd>> {
+    while let Some(ch) = r.read_char()? {
+        buf.push(ch);
+        match ch {
+            '"' => return Ok(Some(ScanEnd::DoubleQuote)),
+            '#' => {
+                let r = classify_hash(r, buf)?;
+                if r.is_some() {
+                    return Ok(r);
+                }
+            }
+            '(' => return Ok(Some(ScanEnd::Paren)),
+            ';' => read_to(r, '\n', buf)?,
+            '|' => return Ok(Some(ScanEnd::Pipe)),
+            _ if is_whitespace(ch) => (),
+            _ => return Ok(Some(ScanEnd::Character)),
+        }
+    }
+    Ok(None)
+}
+
+fn classify_hash(r: &mut dyn CharReader, buf: &mut String) -> PortResult<Option<ScanEnd>> {
+    if let Some(ch) = r.peek_char()? {
+        match ch {
+            '(' => {
+                // NOTE: vector
+                consume_char(r, buf)?;
+                return Ok(Some(ScanEnd::Paren));
+            }
+            ';' => {
+                // NOTE: datum comment, keep going
+                consume_char(r, buf)?;
+            }
+            'u' => {
+                consume_char(r, buf)?;
+                if let Some(ch) = r.peek_char()?
+                    && ch == '8'
+                {
+                    consume_char(r, buf)?;
+                    if let Some(ch) = r.peek_char()?
+                        && ch == '('
+                    {
+                        // NOTE: bytevector
+                        consume_char(r, buf)?;
+                        return Ok(Some(ScanEnd::Paren));
+                    }
+                }
+                return Ok(Some(ScanEnd::Character));
+            }
+            '|' => {
+                consume_char(r, buf)?;
+                loop {
+                    read_to(r, '|', buf)?;
+                    match r.peek_char()? {
+                        None => break,
+                        Some(ch) => {
+                            if ch == '#' {
+                                // NOTE: block comment
+                                consume_char(r, buf)?;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    } else {
+        return Ok(Some(ScanEnd::Character));
+    }
+    Ok(None)
+}
+
+fn read_to(r: &mut dyn CharReader, sentinel: char, buf: &mut String) -> PortResult {
+    while let Some(ch) = r.read_char()? {
+        buf.push(ch);
+        if ch == sentinel {
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn consume_char(r: &mut dyn CharReader, buf: &mut String) -> PortResult {
+    buf.push(r.read_char()?.expect("just-peeked char should be readable"));
+    Ok(())
 }
 
 enum WriteRef<'a> {
